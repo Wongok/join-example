1274f2a5205ca3b795e01981841e3cc8db34cec1
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2019-12-01 23:22:17 +0900

    ADD Domain - User/Board/Post/Comment

---
+++ src/main/java/boot/jpa/join/domain/board/Board.java
@@ -0,0 +1,29 @@
+package boot.jpa.join.domain.board;
+
+import boot.jpa.join.domain.post.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String boardName;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Post> posts = new ArrayList<>();
+
+    @Builder
+    public Board(String name, String email) {
+        this.boardName = boardName;
+    }
+}
---
+++ src/main/java/boot/jpa/join/domain/post/Post.java
@@ -0,0 +1,43 @@
+package boot.jpa.join.domain.post;
+
+import boot.jpa.join.domain.board.Board;
+import boot.jpa.join.domain.comment.Comment;
+import boot.jpa.join.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "FK_POST_BOARD"))
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(foreignKey = @ForeignKey(name = "FK_POST_USER"))
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    public Post(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+}
