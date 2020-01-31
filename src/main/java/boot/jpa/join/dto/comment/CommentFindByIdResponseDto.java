9d7b548e6b8095552573386891f372c901a7ce15
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-01-31 22:50:13 +0900

    Add CommentFindByIdResponseDto

--- src/main/java/boot/jpa/join/dto/comment/CommentFindByIdResponseDto.java
+++ src/main/java/boot/jpa/join/dto/comment/CommentFindByIdResponseDto.java
@@ -1,4 +1,21 @@
 package boot.jpa.join.dto.comment;
 
+import boot.jpa.join.domain.comment.Comment;
+import boot.jpa.join.domain.post.Post;
+import boot.jpa.join.domain.user.User;
+import lombok.Getter;
+
+@Getter
 public class CommentFindByIdResponseDto {
+    private Long id;
+    private String comment;
+    private Post post;
+    private User user;
+
+    public CommentFindByIdResponseDto(Comment comment) {
+        this.id = comment.getId();
+        this.comment = comment.getComment();
+        this.post = comment.getPost();
+        this.user = comment.getUser();
+    }
 }
