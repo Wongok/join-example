0230f041f17fe6f962fd311cd1b55c81c924ebae
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-02-02 23:41:23 +0900

    Modify PostFindByIdResponseDto / modify BoardFindByIdResponseDto

--- src/main/java/boot/jpa/join/dto/board/BoardFindByIdResponseDto.java
+++ src/main/java/boot/jpa/join/dto/board/BoardFindByIdResponseDto.java
@@ -2,21 +2,25 @@ package boot.jpa.join.dto.board;
 
 import boot.jpa.join.domain.board.Board;
 import boot.jpa.join.domain.post.Post;
+import boot.jpa.join.dto.post.PostFindAllResponseDto;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class BoardFindByIdResponseDto {
 
     private Long id;
     private String boardName;
-    private List<Post> posts;
+    private List<PostFindAllResponseDto> posts;
 
     public BoardFindByIdResponseDto(Board board) {
         this.id = board.getId();
         this.boardName = board.getBoardName();
-        this.posts = board.getPosts();
+        this.posts = board.getPosts().stream()
+                .map(PostFindAllResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/boot/jpa/join/dto/post/PostFindByIdResponseDto.java
+++ src/main/java/boot/jpa/join/dto/post/PostFindByIdResponseDto.java
@@ -4,11 +4,13 @@ import boot.jpa.join.domain.board.Board;
 import boot.jpa.join.domain.comment.Comment;
 import boot.jpa.join.domain.post.Post;
 import boot.jpa.join.domain.user.User;
+import boot.jpa.join.dto.comment.CommentFindAllResponseDto;
 import lombok.Getter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class PostFindByIdResponseDto {
@@ -18,7 +20,7 @@ public class PostFindByIdResponseDto {
     private String content;
     private Board board;
     private User user;
-    private List<Comment> comments;
+    private List<CommentFindAllResponseDto> comments;
 
     public PostFindByIdResponseDto(Post post) {
         this.id = post.getId();
@@ -26,6 +28,8 @@ public class PostFindByIdResponseDto {
         this.content = post.getContent();
         this.board = post.getBoard();
         this.user = post.getUser();
-        this.comments = post.getComments();
+        this.comments = post.getComments().stream()
+                .map(CommentFindAllResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
