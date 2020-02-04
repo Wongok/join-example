ae6f01ed3dc39885be24a211c62d69b356d3f56e
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-02-04 21:12:20 +0900

    Add Inner, Cross Join example

--- src/main/java/boot/jpa/join/domain/post/PostRepository.java
+++ src/main/java/boot/jpa/join/domain/post/PostRepository.java
@@ -1,6 +1,19 @@
 package boot.jpa.join.domain.post;
 
+import boot.jpa.join.dto.board.BoardPostDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("SELECT new boot.jpa.join.dto.board.BoardPostDto(b.boardName, p.title, p.content) "
+            + "FROM Board b "
+            + "INNER JOIN b.posts p")
+    List<BoardPostDto> fetchBoardPostInnerJoin();
+    @Query("SELECT new boot.jpa.join.dto.board.BoardPostDto(b.boardName, p.title, p.content) "
+            + "FROM Board b, Post p")
+    List<BoardPostDto> fetchBoardPostCrossJoin();
+
 }
