68bfb2d97546e381ccb730823885c1f60d62c40e
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-02-07 19:22:09 +0900

    Add UserPostInnerJoin, FetchJoin example

--- src/main/java/boot/jpa/join/domain/user/UserRepository.java
+++ src/main/java/boot/jpa/join/domain/user/UserRepository.java
@@ -1,6 +1,23 @@
 package boot.jpa.join.domain.user;
 
+import boot.jpa.join.dto.post.PostFindAllResponseDto;
+import boot.jpa.join.dto.user.UserFindAllResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /*@Query("select p " +
+            "from Post p " +
+            "INNER JOIN p.user u")*/
+    @Query("select u.posts " +
+            "from User u ")
+    List<PostFindAllResponseDto> UserPostInnerJoin();
+
+    @Query("select u " +
+            "from User u " +
+            "JOIN FETCH u.posts")
+    List<UserFindAllResponseDto> FetchJoinUserPost();
 }
