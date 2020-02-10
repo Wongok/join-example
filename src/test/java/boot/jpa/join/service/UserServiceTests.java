4aa9e656987a08a974d58e699ced06c69b43be34
DESKTOP-OSRLKA2\tkddj <tkddjs0116@gmail.com>
2020-02-10 20:33:11 +0900

    Add UserServiceTests (FetchJoin)

---
+++ src/test/java/boot/jpa/join/service/UserServiceTests.java
@@ -0,0 +1,56 @@
+package boot.jpa.join.service;
+
+import boot.jpa.join.domain.board.BoardRepository;
+import boot.jpa.join.domain.comment.CommentRepository;
+import boot.jpa.join.domain.post.PostRepository;
+import boot.jpa.join.domain.user.UserRepository;
+import boot.jpa.join.dto.post.PostFindAllResponseDto;
+import boot.jpa.join.dto.user.UserFindAllResponseDto;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    BoardRepository boardRepository;
+    @Autowired
+    PostRepository postRepository;
+    @Autowired
+    CommentRepository commentRepository;
+
+
+    @After
+    public void cleanUp() {
+        userRepository.deleteAll();
+        boardRepository.deleteAll();
+        postRepository.deleteAll();
+        commentRepository.deleteAll();
+    }
+
+    @Test
+    public void FetchJoinTest() {
+        // given : data-h2.sql
+
+        // when
+        List<UserFindAllResponseDto> list = userService.UserPostFetchJoin();
+
+        //then
+        System.out.println();
+        Assert.assertThat(list.size(), CoreMatchers.is(5));
+    }
+}
